9bfc1714847350a081026d6d6fae4b8edc56a077
MHKhalil1 <khalilmahmoud2333@gmail.com>
2023-01-18 20:03:49 -0500

    About Me File Started

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="title"></h2>
+            <div>
+
+                <p>
+                Hi my name is Mahmoud Khalil I am currently studying as a full stack web-developer with CWRU Boot Camp.
+                </p>
+              
+            </div>
+        </section>
+    )
+}
+
+export default About;
