1c5d8cd5ffda2fbe32d9c7d0cfb76816e48c1655
MHKhalil1 <khalilmahmoud2333@gmail.com>
2023-01-18 20:25:33 -0500

    Contact File Started

---
+++ src/components/Contact/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
+function ContactForm() {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+    
+                if(!isValid) {
+                    setErrorMessage('Enter a valid email');
+                } else {
+                    setErrorMessage('');
+                }
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                } 
+        }
+
+        if (!errorMessage) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+}
+export default ContactForm;
